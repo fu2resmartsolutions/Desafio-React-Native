a69b2cd79642199a5f810967394aba01ab411b99
Thiago Tibau <ttibaudev@gmail.com>
2019-07-31 20:26:24 -0300

    Creating productContainer to add margin

--- src/Screens/Product/styles.js
+++ src/Screens/Product/styles.js
@@ -9,10 +9,16 @@ export const ProductPrice = styled.Text`
     font-size: 30px;
     font-weight: bold;
     color: #00A74D;
+    margin: 10px 0 10px 0;
 `;
 
 export const ProductDetails = styled.Text`
     font-size: 15px;
     color: #4B4B4B;
     text-align: center;
+    margin: 10px 0 10px 0;
+`;
+
+export const ProductContainer = styled.View`
+    margin: 40px;
 `;
